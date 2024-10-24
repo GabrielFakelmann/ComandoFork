0785ed218b8f4ba8d6ad0233b835d1926bd2ff9d
Gabriel Fakelmann <0001068776@senaimgaluno.com.br>
2024-10-24 15:13:00 -0300

    Exercicio 20

--- ConhendoFork/src/Ex20.java
+++ ConhendoFork/src/Ex20.java
@@ -1,11 +1,24 @@
 /*Verificação de Par ou Ímpar: Peça um número ao usuário e informe se ele é par, 
  * ímpar ou neutro.
  */
+import java.util.Scanner;
+
 public class Ex20 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
 
-	}
+        System.out.print("Digite um número inteiro: ");
+            int numero = sc.nextInt();
 
+            if (numero == 0) {
+                System.out.println("O número é neutro.");
+            } else if (numero % 2 == 0) {
+                System.out.println("O número é par.");
+            } else if (numero % 2 != 0){
+                System.out.println("O número é ímpar.");
+            }else {
+            	System.out.println("Valor incorreto ou decimal");
+            }
+        sc.close();
+    }
 }
